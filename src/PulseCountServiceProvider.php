d7f13d3e6af8cd093d76852bfb166fb00b69454e
Abbas mkhzomi <amkhzomi@gmail.com>
2023-12-17 00:03:29 +0330

    add service provider

---
+++ src/PulseCountServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Abbasudo\PulseCount;
+
+use Abbasudo\PulseCount\Livewire\PulseCount;
+use Illuminate\Contracts\Foundation\Application;
+use Livewire\LivewireManager;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class PulseCountServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('pulse-count')
+            ->hasViews('pulse-count');
+    }
+
+    public function boot(): void
+    {
+        parent::boot();
+
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'pulse-count');
+
+        $this->callAfterResolving('livewire', function (LivewireManager $livewire, Application $app) {
+            $livewire->component('pulse.usage-count', PulseCount::class);
+        });
+    }
+}
